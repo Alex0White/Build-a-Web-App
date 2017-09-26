4dfbfc4877877215bbe8d180b338a3298fecebb8
Konnor <connorjrw@hotmail.co.nz>
2017-09-26 20:31:13 +1300

    created tables

--- assignmentTwo.go
+++ assignmentTwo.go
@@ -1,6 +1,50 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
 func main() {
+	prepareDatabase()
+
+}
+
+func prepareDatabase() {
+	db, err := sql.Open("postgres", "user=postgres password=admin dbname=webAppDatabase sslmode=disable")
+	err = db.Ping()
+
+	//userstable
+	_, err = db.Exec("DROP TABLE IF EXISTS UsersTable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("CREATE TABLE UsersTable(username varchar(50), password varchar(50))")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//notestable
+	_, err = db.Exec("DROP TABLE IF EXISTS NotesTable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec("CREATE TABLE NotesTable(noteId int, username varchar(50), note varchar(50))")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//permissions table
+	_, err = db.Exec("DROP TABLE IF EXISTS PermissionsTable ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE PermissionsTable(noteId int, username varchar(50), read boolean, write boolean)")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
 
