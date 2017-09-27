4c3e5b9540848c71cd6f0efad6e85a0c1d69be75
Alex0White <alexjwhitex@gmail.com>
2017-09-27 15:15:03 +1300

    post
    not quite working

--- basic-webthing.go
+++ basic-webthing.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -19,8 +20,47 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello Alex!")
 }
 
+func sayhelloConnor(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello Connor!")
+}
+func sayhelloPost(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	fmt.Println(decoder)
+	data := struct {
+		Message string `json:"msg"`
+	}{}
+
+	err := decoder.Decode(&data)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(data)
+	defer r.Body.Close()
+	fmt.Fprintf(w, string(data.Message))
+}
+
 func main() {
 	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/c", sayhelloConnor)
+	http.HandleFunc("/newnote", sayhelloPost)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
